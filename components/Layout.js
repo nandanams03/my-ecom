c426a2f23029df8ee06dcf93db50f356cd6db8c2
nandanams03 <nandanams03@gmail.com>
2023-11-04 21:08:25 +0530

    first commit

---
+++ components/Layout.js
@@ -0,0 +1,14 @@
+// components/Layout.js
+
+import Navbar from './Navbar';
+import Footer from './Footer';
+const Layout = ({ children }) => {
+ return (
+    <div>
+       <Navbar />
+       {children}
+       <Footer />
+    </div>
+ );
+};
+export default Layout;
